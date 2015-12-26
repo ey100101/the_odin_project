b1018bec747446f855f2e9722129a393691f31db
Emily Yu <ey100101@gmail.com>
2015-12-26 14:58:56 -0500

    slider done

---
+++ slider/js/slider.js
@@ -0,0 +1,60 @@
+sliderInt = 1;
+sliderNext = 2;
+
+$(document).ready(function() {
+  $("#slider > Img#1").fadeIn(300);
+  startSlider();
+});
+
+function startSlider() {
+  count = $("#slider > Img").size();
+
+  loop = setInterval(function() {
+    $("#slider > Img").fadeOut(300);
+    $("#slider > Img#" + sliderNext).fadeIn(300);
+
+    sliderInt = sliderNext;
+    sliderNext = sliderNext + 1;
+  }, 3000)
+}
+
+function prev() {
+  newSlide = sliderInt - 1;
+  showSlide(newSlide);
+}
+
+function next() {
+  newSlide = sliderInt + 1;
+  showSlide(newSlide);
+}
+
+function stopLoop() {
+    window.clearInterval(loop);
+}
+
+function showSlide(Id) {
+  stopLoop();
+
+  if(Id > count) {
+    Id = 1;
+  } else if(Id < 1) {
+    Id = count;
+  }
+
+  $("#slider > Img").fadeOut(300);
+  $("#slider > Img#" + Id).fadeIn(300);
+
+  sliderInt = Id;
+  sliderNext = Id + 1;
+
+  startSlider();
+}
+
+$("#slider > Img").hover(
+  function() {
+    stopLoop();
+  },
+  function() {
+    startSlider();
+  }
+)
